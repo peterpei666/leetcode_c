6414470a356a748cb1697aafbcadbb208296d5a0
Peter_Pei <mmiaomiao140@gmail.com>
2025-04-02 22:09:41 +0800

    Add files via upload

---
+++ 2873. Maximum Value of an Ordered Triplet I.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long maximumTripletValue(int* nums, int numsSize)
+{
+    int i;
+    long long temp,max=0;
+    int* leftmax=(int*)malloc(sizeof(int)*numsSize);
+    int* rightmax=(int*)malloc(sizeof(int)*numsSize);
+    leftmax[0]=nums[0];
+    rightmax[numsSize-1]=nums[numsSize-1];
+    for(i=1;i<numsSize;i++)
+    {
+        if(nums[i]>leftmax[i-1])
+        {
+            leftmax[i]=nums[i];
+        }
+        else
+        {
+            leftmax[i]=leftmax[i-1];
+        }
+    }
+    for(i=numsSize-2;i>=0;i--)
+    {
+        if(nums[i]>rightmax[i+1])
+        {
+            rightmax[i]=nums[i];
+        }
+        else
+        {
+            rightmax[i]=rightmax[i+1];
+        }
+    }
+    for(i=1;i<numsSize-1;i++)
+    {
+        temp=((long long)(leftmax[i-1]-nums[i]))*(long long)rightmax[i+1];
+        if(max<temp)
+        {
+            max=temp;
+        }
+    }
+    free(leftmax);
+    free(rightmax);
+    return max;
+}
