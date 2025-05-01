b1e840c34b7dfad102d05020f67c99e3ac9fd701
Peter_Pei <mmiaomiao140@gmail.com>
2025-05-01 21:59:03 +0800

    Add files via upload

---
+++ 3392. Count Subarrays of Length Three With a Condition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countSubarrays(int* nums, int numsSize)
+{
+    int i,count=0;
+    for(i=0;i<numsSize-2;i++)
+    {
+        if(2*(nums[i]+nums[i+2])==nums[i+1])
+        {
+            count++;
+        }
+    }
+    return count;
+}
