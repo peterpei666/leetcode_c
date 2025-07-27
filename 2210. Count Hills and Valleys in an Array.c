d92855b690b862ddf18a0ef5bd5bc9f0eddf32f6
Peter_Pei <mmiaomiao140@gmail.com>
2025-07-27 23:46:59 +0800

    Add files via upload

---
+++ 2210. Count Hills and Valleys in an Array.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countHillValley(int* nums, int n)
+{
+    int cnt=0;
+    for(int i=1;i<n-1;i++)
+    {
+        if(nums[i]==nums[i-1])
+        {
+            continue;
+        }
+        int l=0;
+        for(int j=i-1;j>=0;j--)
+        {
+            if(nums[j]<nums[i])
+            {
+                l=1;
+                break;
+            }
+            else if(nums[j]>nums[i])
+            {
+                l=-1;
+                break;
+            }
+        }
+        int r=0;
+        for(int j=i+1;j<n;j++)
+        {
+            if(nums[j]<nums[i])
+            {
+                r=1;
+                break;
+            }
+            else if(nums[j]>nums[i])
+            {
+                r=-1;
+                break;
+            }
+        }
+        if(l==r)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
