9e96fef7c2cf347b64404eb11281c465c7ead1a9
Peter_Pei <mmiaomiao140@gmail.com>
2025-08-13 23:33:03 +0800

    Add files via upload

---
+++ 326. Power of Three.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool isPowerOfThree(int n)
+{
+    if(n<=0)
+    {
+        return false;
+    }
+    while(n%3==0)
+    {
+        n/=3;
+    }
+    return n==1;
+}
