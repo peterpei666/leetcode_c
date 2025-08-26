24f9efce3ecfcd29e933c238fdec8e7b92e28e64
Peter_Pei <mmiaomiao140@gmail.com>
2025-08-26 22:49:18 +0800

    Add files via upload

---
+++ 410. Split Array Largest Sum.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static inline int max(int a,int b)
+{
+    return a>b?a:b;
+}
+
+int splitArray(int* nums, int n, int k)
+{
+    int l=nums[0];
+    int pre[n];
+    pre[0]=nums[0];
+    for(int i=1;i<n;i++)
+    {
+        pre[i]=pre[i-1]+nums[i];
+        l=max(l,nums[i]);
+    }
+    int r=pre[n-1];
+    while(l<r)
+    {
+        int mid=(l+r)/2;
+        int cnt=1,prev=0;
+        for(int i=0;i<n;i++)
+        {
+            if(pre[i]-prev>mid)
+            {
+                cnt++;
+                prev=pre[i-1];
+            }
+        }
+        if(cnt>k)
+        {
+            l=mid+1;
+        }
+        else
+        {
+            r=mid;
+        }
+    }
+    return l;
+}
