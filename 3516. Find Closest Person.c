75126a53c2fef3bc3a2bf979d4ee37f09ef94f42
Peter_Pei <mmiaomiao140@gmail.com>
2025-09-04 23:08:34 +0800

    Add files via upload

---
+++ 3516. Find Closest Person.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findClosest(int x, int y, int z)
+{
+    int a = abs(x - z);
+    int b = abs(y - z);
+    if (a == b)
+    {
+        return 0;
+    }
+    else if (a > b)
+    {
+        return 2;
+    }
+    else
+    {
+        return 1;
+    }
+}
