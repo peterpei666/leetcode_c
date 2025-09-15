b3f6ddbda5e2c9aadad24c608e9204385253a82d
Peter_Pei <mmiaomiao140@gmail.com>
2025-09-15 23:43:46 +0800

    Add files via upload

---
+++ 600. Non-negative Integers without Consecutive Ones.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findIntegers(int n)
+{
+    int f[32];
+    f[0] = 1;
+    f[1] = 2;
+    for (int i = 2; i < 32; i++)
+    {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+    int i = 30;
+    int ans = 0;
+    int prev = 0;
+    while (i >= 0)
+    {
+        if (n & (1 << i))
+        {
+            ans += f[i];
+            if (prev == 1)
+            {
+                ans--;
+                break;
+            }
+            prev = 1;
+        }
+        else
+        {
+            prev = 0;
+        }
+        i--;
+    }
+    return ans + 1;
+}
