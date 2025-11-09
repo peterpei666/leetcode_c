8961669c28067b756c359fea10d0aba5a6a78f25
Peter_Pei <mmiaomiao140@gmail.com>
2025-11-09 23:29:35 +0800

    Add files via upload

--- 2169. Count Operations to Obtain Zero.c
+++ 2169. Count Operations to Obtain Zero.c
@@ -3,18 +3,18 @@
 
 int countOperations(int num1, int num2)
 {
-    int cnt=0;
-    while(num1&&num2)
+    int ans = 0;
+    while (num1 && num2)
     {
-        cnt++;
-        if(num1>=num2)
+        if (num1 >= num2)
         {
-            num1-=num2;
+            num1 -= num2;
         }
         else
         {
-            num2-=num1;
+            num2 -= num1;
         }
+        ans++;
     }
-    return cnt;
+    return ans;
 }
