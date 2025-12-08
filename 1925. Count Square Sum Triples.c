a3c53d464f9b1204a276d83fd71980a414261692
Peter_Pei <mmiaomiao140@gmail.com>
2025-12-08 22:16:20 +0800

    Add files via upload

---
+++ 1925. Count Square Sum Triples.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countTriples(int n)
+{
+    int ans = 0;
+    for (int a = 1; a < n; a++)
+    {
+        for (int b = a + 1; b < n; b++)
+        {
+            for (int c = b + 1; c <= n; c++)
+            {
+                if (a * a + b * b == c * c)
+                {
+                    ans += 2;
+                }
+            }
+        }
+    }
+    return ans;
+}
