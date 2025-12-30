0ebbf3b62b0f88f3c2f8c9fdb3ae1a5d65842d4c
Peter_Pei <mmiaomiao140@gmail.com>
2025-12-30 22:50:05 +0800

    Add files via upload

---
+++ 840. Magic Squares In Grid.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool isMagic(int** grid, int x, int y)
+{
+    bool mp[9];
+    for (int i = -1; i < 2; i++)
+    {
+        for (int j = -1; j < 2; j++)
+        {
+            if (grid[x + i][y + j] > 9 || grid[x + i][y + j] == 0 || mp[grid[x + i][y + j] - 1])
+            {
+                return false;
+            }
+            else
+            {
+                mp[grid[x + i][y + j] - 1] = true;
+            }
+        }
+    }
+    int t = grid[x - 1][y - 1] + grid[x][y] + grid[x + 1][y + 1];
+    if (grid[x - 1][y + 1] + grid[x][y] + grid[x + 1][y - 1] != t)
+    {
+        return false;
+    }
+    for (int i = -1; i < 2; i++)
+    {
+        if (grid[x + i][y - 1] + grid[x + i][y] + grid[x + i][y + 1] != t || grid[x - 1][y + i] + grid[x][y + i] + grid[x + 1][y + i] != t)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int numMagicSquaresInside(int** grid, int gridSize, int* gridColSize)
+{
+    int m = gridSize;
+    int n = gridColSize[0];
+    int ans = 0;
+    for (int i = 1; i < m - 1; i++)
+    {
+        for (int j = 1; j < n - 1; j++)
+        {
+            if (isMagic(grid, i, j))
+            {
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
