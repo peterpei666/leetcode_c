4b8f3319dbb6adde1b3df056c3a733efd40eca20
Peter_Pei <mmiaomiao140@gmail.com>
2026-01-12 22:42:22 +0800

    Add files via upload

--- 1266. Minimum Time Visiting All Points.c
+++ 1266. Minimum Time Visiting All Points.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-inline int max(int a,int b)
+static inline int max(int a, int b)
 {
-    return a>b?a:b;
+    return a > b ? a : b;
 }
 
 int minTimeToVisitAllPoints(int** points, int n, int* pointsColSize)
 {
-    int ans=0;
-    int i;
-    for(i=1;i<n;i++)
+    int ans = 0;
+    for (int i = 1; i < n; i++)
     {
-        ans+=max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]));
+        ans += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
     }
     return ans;
 }
