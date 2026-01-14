5aff98197aae34f16af63520f98bdd3e44c27db5
Peter_Pei <mmiaomiao140@gmail.com>
2026-01-14 23:38:30 +0800

    Add files via upload

---
+++ 1266. Minimum Time Visiting All Points.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points)
+    {
+        int n = (int)points.size();
+        int ans = 0;
+        for (int i = 1; i < n; i++)
+        {
+            ans += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
+        }
+        return ans;
+    }
+};
