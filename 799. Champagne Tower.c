f5be38b622c4587f0be76cb16d0d681305a6c6dc
Peter_Pei <mmiaomiao140@gmail.com>
2026-02-14 23:21:45 +0800

    Add files via upload

---
+++ 799. Champagne Tower.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static inline double max(double a, double b)
+{
+    return a > b ? a : b;
+}
+
+static inline double min(double a, double b)
+{
+    return a < b ? a : b;
+}
+
+double champagneTower(int poured, int query_row, int query_glass)
+{
+    static double dp[2][101];
+    for (int i = 0; i <= 100; i++)
+    {
+        dp[0][i] = 0.0;
+        dp[1][i] = 0.0;
+    }
+    dp[0][0] = poured;
+    for (int i = 1; i <= query_row; i++)
+    {
+        dp[i & 1][0] = max(dp[(i - 1) & 1][0] - 1.0, 0.0) / 2;
+        for (int j = 1; j <= query_row; j++)
+        {
+            dp[i & 1][j] = max(dp[(i - 1) & 1][j - 1] - 1.0, 0.0) / 2 + max(dp[(i - 1) & 1][j] - 1.0, 0.0) / 2;
+        }
+    }
+    return min(1.0, dp[query_row & 1][query_glass]);
+}
