38b7824085d2e7924e84e23ae8794de47a52b44c
Peter_Pei <mmiaomiao140@gmail.com>
2026-03-11 23:36:30 +0800

    Add files via upload

---
+++ 476. Number Complement.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findComplement(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int ans = 0;
+    for (int i = 0; i < 31 && (1 << i) <= n; i++)
+    {
+        if ((n & (1 << i)) == 0)
+        {
+            ans |= 1 << i;
+        }
+    }
+    return ans;
+}
